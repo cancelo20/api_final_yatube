3246330a4c6bd488a1e9f4f6c9a6f211ce1b3362
Степан Крылов <kiriki00@mail.ru>
2023-02-04 17:31:39 +0700

    visual fixes

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -5,7 +5,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('redoc/',
+    path(
+        'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'),
 ]
